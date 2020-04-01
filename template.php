42d0b57926fa55fa9ea016341ed1e6dd21e92405
jtltennis <31020968+jtltennis@users.noreply.github.com>
2020-04-01 09:28:14 -0400

    Update template.php

--- template.php
+++ template.php
@@ -18,8 +18,8 @@ $pdf->addPage('L', [792, 612]);
 $pdf->useTemplate($tplIdx);
 
 // Set font
-$pdf->AddFont('oldenglish');
-$pdf->SetFont('oldenglish', '', 48);
+$pdf->AddFont('diploma');
+$pdf->SetFont('diploma', '', 48);
 // Set color
 $pdf->SetTextColor(35, 31, 32);
 // Move to 500 points from the top
