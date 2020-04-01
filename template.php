af967f6b09829a3f203059001408958f7f12ff86
jtltennis <31020968+jtltennis@users.noreply.github.com>
2020-04-01 09:40:20 -0400

    WORK

--- template.php
+++ template.php
@@ -18,8 +18,8 @@ $pdf->addPage('L', [792, 612]);
 $pdf->useTemplate($tplIdx);
 
 // Set font
-$pdf->AddFont('helvetica');
-$pdf->SetFont('helvetica', '', 48);
+$pdf->AddFont('Ancient-Medium');
+$pdf->SetFont('Ancient-Medium', '', 48);
 // Set color
 $pdf->SetTextColor(35, 31, 32);
 // Move to 500 points from the top
